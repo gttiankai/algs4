8447d088d8c7f77609930ce68f8fced8a5a4888c
Tian Kai <gttiankai@163.com>
2019-06-21 20:36:20 +0800

    Update: 添加了排序算法得定义

---
+++ src/src/com/sort/Merge.java
@@ -0,0 +1,20 @@
+package com.sort;
+
+import java.util.Comparator;
+
+public class Merge {
+    public static void sort(Comparable[] a) {
+
+    }
+
+    private static boolean less(Comparator v, Comparator w) {
+
+    }
+    private static void exch(Object[] a, int i, int j) {
+
+    }
+    private static boolean isSorted(Comparator[] a, int lo, int hi) {
+
+    }
+}
+}
---
+++ src/src/com/sort/Quick.java
@@ -0,0 +1,21 @@
+package com.sort;
+
+import java.util.Comparator;
+
+public class Quick {
+
+    public static void sort(Comparable[] a) {
+
+    }
+
+    private static boolean less(Comparator v, Comparator w) {
+
+    }
+    private static void exch(Object[] a, int i, int j) {
+
+    }
+    private static boolean isSorted(Comparator[] a, int lo, int hi) {
+
+    }
+}
+}
